a6b255582a2ea8f3d95084402ea2fc1c892e48b2
Imene <dsin318@gmail.com>
2019-11-01 22:12:51 -0400

    Ajout du Modele CLIENT ajout modif sup

--- src/main/java/guru/springframework/controllers/ProductController.java
+++ src/main/java/guru/springframework/controllers/ProductController.java
@@ -9,51 +9,52 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+/**
+ * Created by jt on 11/6/15.
+ */
 @Controller
 public class ProductController {
+
     private ProductService productService;
 
     @Autowired
     public void setProductService(ProductService productService) {
         this.productService = productService;
     }
-    @RequestMapping("/product/{id}")
-    public String getProduct(@PathVariable Integer id, Model model){
-model.addAttribute("product",productService.getProductById(id));
-return "product";
-    }
-    @RequestMapping("/products")
+
+    @RequestMapping("/product/list")
     public String listProducts(Model model){
+        model.addAttribute("products", productService.listAll());
+        return "product/list";
+    }
 
-        model.addAttribute("products", productService.listAllProducts());
+    @RequestMapping("/product/show/{id}")
+    public String getProduct(@PathVariable Integer id, Model model){
+        model.addAttribute("product", productService.getById(id));
+        return "product/show";
+    }
 
-        return "products";
+    @RequestMapping("product/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model){
+        model.addAttribute("product", productService.getById(id));
+        return "product/productform";
     }
 
     @RequestMapping("/product/new")
     public String newProduct(Model model){
-
         model.addAttribute("product", new Product());
-
-        return "productform";
-    }
-    @RequestMapping("product/edit/{id}")
-    public String edit(@PathVariable Integer id, Model model){
-        model.addAttribute("product", productService.getProductById(id));
-        return "productform";
+        return "product/productform";
     }
 
-    @RequestMapping(value = "product", method = RequestMethod.POST)
+    @RequestMapping(value = "/product", method = RequestMethod.POST)
     public String saveOrUpdateProduct(Product product){
-
-        Product prodsave = productService.saveOrUpdateProduct(product);
-
-        return "redirect:/product/" + prodsave.getId();
+        Product savedProduct = productService.saveOrUpdate(product);
+        return "redirect:/product/show/" + savedProduct.getId();
     }
 
     @RequestMapping("/product/delete/{id}")
     public String delete(@PathVariable Integer id){
-productService.deleteProduct(id);
-        return "redirect:/products";
+        productService.delete(id);
+        return "redirect:/product/list";
     }
 }
--- src/main/java/guru/springframework/services/ProductService.java
+++ src/main/java/guru/springframework/services/ProductService.java
@@ -4,13 +4,5 @@ import guru.springframework.domain.Product;
 
 import java.util.List;
 
-public interface ProductService {
-
-    List<Product> listAllProducts();
-
-    Product getProductById(Integer id);
-
-    Product saveOrUpdateProduct(Product product);
-
-    void deleteProduct(Integer id);
+public interface ProductService extends CRUDService<Product> {
 }

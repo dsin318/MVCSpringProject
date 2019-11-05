cd837303d8643bd22eff4ff61f8ae9b1a1784447
Imene <dsin318@gmail.com>
2019-11-04 22:27:22 -0500

    Adding services using Entity manager For Customer Object and Test with JUNIT

--- src/main/java/guru/springframework/services/AbstractMapService.java
+++ src/main/java/guru/springframework/services/AbstractMapService.java
@@ -12,7 +12,7 @@ public abstract class AbstractMapService  {
 
     public AbstractMapService() {
         domainMap = new HashMap<>();
-        loadDomainObjects();
+
     }
 
     public List<DomainObject> listAll() {
@@ -42,9 +42,10 @@ public abstract class AbstractMapService  {
     }
 
     private Integer getNextKey(){
+        if(domainMap.size()==0)
+            return 1;
         return Collections.max(domainMap.keySet()) + 1;
     }
 
-    protected abstract void loadDomainObjects();
 
 }
